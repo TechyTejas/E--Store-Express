65d1c33f5ebf0c99949e01782985f7f9ba4df9ba
tejas <sadadetej.27@gmail.com>
2023-12-10 01:01:11 +0530

    Dynamic Routes 2 Post

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require ('../models/cart')
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -11,18 +12,28 @@ exports.getProducts = (req, res, next) => {
 };
 
 // for details page getting id 
-exports.getProduct = (req,res,next)=> {
-  const prodId = req.params.productId
-  Product.findById(prodId, product => {
-   res.render('shop/product-detail', {product : product,
-  pageTitle : product.title,
-  path: '/products'
-})
-  })
-  // console.log(prodId)
-  // res.redirect('/')
-}
+// exports.getProduct = (req,res,next)=> {
+//   const prodId = req.params.productId
+//   Product.findById(prodId, product => {
+//    res.render('shop/product-detail', {product : product,
+//   pageTitle : product.title,   // adding template literals
+//   path: '/products'
+// })
+//   })
+//   // console.log(prodId)
+//   // res.redirect('/')
+// }
 
+  exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    });
+  };
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -40,6 +51,18 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+
+exports.postCart = (req,res,next) => {
+  console.log(req.body + " i am tejas smart boy ")
+  const prodId=req.body.productId;
+  Product.findById(prodId, product=>{
+    Cart.addProduct(prodId, product.price);
+  });
+  console.log(prodId)
+  res.redirect('/cart')
+}
+
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
